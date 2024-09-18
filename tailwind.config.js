8bb85c2866155ce8ea8d9d161fd7f28bcdc7631d
Junming <guan.junming85@gmail.com>
2024-09-18 22:53:13 +0800

    test deploy

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#f4d04e",
+        "gray-950": "#111111",
+        "gray-500": "#6b6b6b",
+      },
+    },
+  },
+  plugins: [],
+};
